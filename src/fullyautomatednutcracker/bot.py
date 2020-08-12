9903c8bd2fc1db329bce5168743e9105ae329926
nwunderly <48489521+nwunderly@users.noreply.github.com>
2020-08-11 22:01:50 -0400

    admin.py, checks.py, is_owner
    admin.py
     - basic management commands
     - commands work for lifeguards
    
    checks.py
     - is_owner: intended to override bot.is_owner for jishaku
     - is_admin: decorator for admin-only commands

--- src/fullyautomatednutcracker/bot.py
+++ src/fullyautomatednutcracker/bot.py
@@ -3,6 +3,8 @@ import datetime
 
 from discord.ext import commands
 
+from utils.checks import is_owner
+
 
 logger = logging.getLogger(__name__)
 handler = logging.FileHandler('../logs/{}.log'.format(str(datetime.datetime.now()).replace(' ', '_').replace(':', 'h', 1).replace(':', 'm').split('.')[0][:-2]))
@@ -13,9 +15,10 @@ logger.setLevel(logging.INFO)
 logging.basicConfig(level=logging.INFO)
 
 logging.info("Starting \"Fully Automated Nutcracker\" bot script!")
-_pfx = ["!"]
+_pfx = ["$", "dmb ", "<:daddy:590058725252005888> "]
 bot = commands.Bot(command_prefix=_pfx)
 bot.help_command = commands.MinimalHelpCommand()
+# bot.is_owner = is_owner  # TODO: do we want this? @dove
 version = "v0.0.1b"
 
 START_TIME = datetime.datetime.now()
