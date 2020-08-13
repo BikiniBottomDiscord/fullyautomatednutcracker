449892661658ab7f02c6825d43bc1e752f100c73
minecrosters <48176165+minecrosters@users.noreply.github.com>
2020-08-13 13:26:53 -0400

    edited

--- src/fullyautomatednutcracker/cogs/fun.py
+++ src/fullyautomatednutcracker/cogs/fun.py
@@ -19,29 +19,32 @@ class Fun(commands.Cog):
     @commands.command(aliases=["rps"])
     async def rockpaperscissors(self, ctx):
         """A nice game of Rock Paper Scissors. - Alias: rps"""
-        VERIFY = False
-        member = ctx.author
+
+        answers = ['rock', 'paper', 'scissors']
         await ctx.send('Thinking of my answer ...')
         await asyncio.sleep(0.8)
         await ctx.send('Got it! Awaiting response.')
-        MESSAGE = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author)
+        try:
+            MESSAGE = await self.bot.wait_for('message', check=lambda message: message.author == ctx.author and message.channel == ctx.channel and message.content in answers, timeout=30.0)
+        except asyncio.TimeoutError:
+            await ctx.send(f'{ctx.author} took too long to respond, game over')
         RESPONSE = random.randint(0, 2)
         await ctx.send(OPTIONS[RESPONSE])
         await asyncio.sleep(0.6)
-        if MESSAGE.content == "rock" and RESPONSE == 2 or MESSAGE.content == "Rock" and RESPONSE == 2:
-            await ctx.send("https://tenor.com/view/the-goon-win-you-won-willy-wonka-oompa-loompa-fc-gif-14046847")
-        elif MESSAGE.content == "paper" and RESPONSE == 0 or MESSAGE.content == "Paper" and RESPONSE == 0:
-            await ctx.send("https://tenor.com/view/the-goon-win-you-won-willy-wonka-oompa-loompa-fc-gif-14046847")
-        elif MESSAGE.content == "scissors" and RESPONSE == 1 or MESSAGE.content == "Scissors" and RESPONSE == 1:
-            await ctx.send("https://tenor.com/view/the-goon-win-you-won-willy-wonka-oompa-loompa-fc-gif-14046847")
-        elif MESSAGE.content == "rock" and RESPONSE == 0 or MESSAGE.content == "Rock" and RESPONSE == 0:
+        if MESSAGE.content.lower() == "rock" and RESPONSE == 2:
+            await ctx.send("https://cdn.discordapp.com/attachments/742559349750235136/743267535876653528/you_win.gif")
+        elif MESSAGE.content.lower() == "paper" and RESPONSE == 0:
+            await ctx.send("https://cdn.discordapp.com/attachments/742559349750235136/743267535876653528/you_win.gif")
+        elif MESSAGE.content.lower() == "scissors" and RESPONSE == 1:
+            await ctx.send("https://cdn.discordapp.com/attachments/742559349750235136/743267535876653528/you_win.gif")
+        elif MESSAGE.content.lower() == "rock" and RESPONSE == 0:
             await ctx.send("https://tenor.com/view/monty-python-draw-gif-5447899")
-        elif MESSAGE.content == "paper" and RESPONSE == 1 or MESSAGE.content == "Paper" and RESPONSE == 1:
+        elif MESSAGE.content.lower() == "paper" and RESPONSE == 1:
             await ctx.send("https://tenor.com/view/monty-python-draw-gif-5447899")
-        elif MESSAGE.content == "scissors" and RESPONSE == 2 or MESSAGE.content == "Scissors" and RESPONSE == 2:
+        elif MESSAGE.content.lower() == "scissors" and RESPONSE == 2:
             await ctx.send("https://tenor.com/view/monty-python-draw-gif-5447899")
         else:
-            await ctx.send("https://tenor.com/view/you-lose-good-day-sir-gif-7465431")
+            await ctx.send("https://cdn.discordapp.com/attachments/742559349750235136/743267433615196170/winner.gif")
 
 
 def setup(bot):
