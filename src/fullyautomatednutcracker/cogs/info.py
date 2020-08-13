2eda50ff1568c06ea683ef884208f3676bf4bed0
minecrosters <48176165+minecrosters@users.noreply.github.com>
2020-08-13 14:06:41 -0400

    edited fun.py (#4)
    * added AntiSelfDeprecation cog and help command class
    
    * Update communityhelpcommand.py
    
    * added antiselfdeprecation cog and custom help command class
    
    and in the right way this time
    
    * Delete antiselfdeprecation.py
    
    * Delete communityhelpcommand.py
    
    * edited
    
    * added av command to info.py
    
    idk if thats the right place to put it tbh
    
    Co-authored-by: dovedevic <dovedevic@users.noreply.github.com>
    Co-authored-by: nwunderly <48489521+nwunderly@users.noreply.github.com>

--- src/fullyautomatednutcracker/cogs/info.py
+++ src/fullyautomatednutcracker/cogs/info.py
@@ -17,6 +17,14 @@ class Info(commands.Cog):
         join_pos = order.index(member) + 1
         await ctx.send(join_pos)
 
+    @commands.command(aliases=['av', 'pfp'])
+    async def avatar(self, ctx, member: discord.Member = None):
+        member = member if member else ctx.author
+        embed = discord.Embed(title='Avatar', color=member.color)
+        embed.set_author(name=str(member), icon_url=member.avatar_url)
+        embed.set_image(url=member.avatar_url)
+        await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Info(bot))
