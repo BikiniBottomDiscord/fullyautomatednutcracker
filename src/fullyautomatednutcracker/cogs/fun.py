b55a6e486e253385c27e5b94000289f4483bbbb9
AxoManAxo <52941588+AxoManAxo@users.noreply.github.com>
2020-08-23 13:12:53 -0500

    added things

--- src/fullyautomatednutcracker/cogs/fun.py
+++ src/fullyautomatednutcracker/cogs/fun.py
@@ -67,7 +67,31 @@ class Fun(commands.Cog):
         embed.set_image(url=post.url)
         embed.set_footer(text='*if image isn\'t working then it probably is a video*')
         await ctx.send(embed=embed)
+    
+    
+    @commands.command(aliases='mc')
+    async def membercount(self,ctx):
+        await ctx.send(f'The Current Member Count Is: {len(ctx.guild.members)}')
 
+    @commands.command(aliases='gi')
+    async def guildinfo(self,ctx):
+        bots = len([x for x in ctx.guild.members if x.bot])
+        amtuser = len([x for x in ctx.guild.members if not x.bot])
+        embed = discord.Embed(title=(f'{ctx.guild.name}\'s info'), colour=discord.Color(0x67fafb),timestamp=ctx.message.created_at)
+        embed.set_footer(text=f"requested by {ctx.author}", icon_url=ctx.author.avatar_url)
+        embed.set_thumbnail(url=ctx.guild.icon_url)
+        embed.add_field(name='Guild id:', value=ctx.guild.id)
+        embed.add_field(name="Members:", value=(f"{amtuser}"))
+        embed.add_field(name="Bots:", value=(f'{bots}'))
+        embed.add_field(name='Owner:', value=(ctx.guild.owner))
+        embed.add_field(name='Roles:', value=(f'{(len(ctx.guild.roles))}'))
+        embed.add_field(name="Guild created at:", value=(f'{ctx.guild.created_at.strftime("%Y/%M/%d")}'))
+        embed.add_field(name='Total Channels:', value=(f'{len(ctx.guild.channels)}'))
+        embed.add_field(name='Total text channels:', value=(f'{len(ctx.guild.text_channels)}'))
+        embed.add_field(name='Total voice channels:', value=(f'{len(ctx.guild.voice_channels)}'))
+        embed.add_field(name='Total number of emojis', value=(f'{len(ctx.guild.emojis)}'))
+        embed.add_field(name='Boost level:', value=(f'{ctx.guild.premium_tier}'))
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Fun(bot))
