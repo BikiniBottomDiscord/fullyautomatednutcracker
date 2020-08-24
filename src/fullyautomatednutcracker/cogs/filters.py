5346b8e15d545f1b8a4d47c2ba9925e46b480c6f
dovedevic <dovedevic@gmail.com>
2020-08-24 04:15:21 -0400

    properlly reized emojis, added emoji filter support

--- src/fullyautomatednutcracker/cogs/filters.py
+++ src/fullyautomatednutcracker/cogs/filters.py
@@ -21,23 +21,48 @@ class Filters(commands.Cog):
         await channel.send(file=discord.File(bytes_fp, file_name))
 
     @commands.command(aliases=["gay", "rainbow"])
-    async def gaygaygay(self, ctx, member: discord.Member =None):
-        member = ctx.author if not member else member
-        downloaded_pfp = image_from_url(member.avatar_url, "image/png")
-        resized_pfp = resize(downloaded_pfp, 250)
-        filter = Image.open('content/filters/gaygaygay.png')
-        resized_filter = resize(filter, 250)
-        resized_pfp.paste(resized_filter, (0, 0), resized_filter)
-        await self.save_img_and_send(resized_pfp, ctx.channel, file_name="gaygaygay.png")
-        downloaded_pfp.close()
-        filter.close()
-        resized_pfp.close()
-        resized_filter.close()
+    async def gaygaygay(self, ctx, inv_object: typing.Union[discord.Member, discord.Emoji]=None):
+        if not inv_object or isinstance(inv_object, discord.Member):
+            member = inv_object or ctx.author
+            downloaded_pfp = image_from_url(member.avatar_url, "image/png")
+            resized_pfp = resize(downloaded_pfp, 250)
+            gay_filter = Image.open('content/filters/gaygaygay.png')
+            resized_filter = resize(gay_filter, 250)
+            resized_pfp.paste(resized_filter, (0, 0), resized_filter)
+            await self.save_img_and_send(resized_pfp, ctx.channel, file_name="gaygaygay.png")
+            downloaded_pfp.close()
+            gay_filter.close()
+            resized_pfp.close()
+            resized_filter.close()
+        else:
+            downloaded_emoji = image_from_url(inv_object.url, "image/png")
+
+            bands = downloaded_emoji.split()
+            width_diff = abs(downloaded_emoji.size[0] - 250)
+            height_diff = abs(downloaded_emoji.size[1] - 250)
+            if height_diff > width_diff:
+                size = (250, downloaded_emoji.size[1] * 250 // downloaded_emoji.size[0])
+            else:
+                size = (downloaded_emoji.size[0] * 250 // downloaded_emoji.size[1], 250)
+            bands = [b.resize(size, Image.LINEAR) for b in bands]
+            resized_emoji = Image.merge('RGBA', bands)
+
+            gay_filter = Image.open('content/filters/gaygaygay.png')
+            bands = gay_filter.split()
+            bands = [b.resize(resized_emoji.size, Image.LINEAR) for b in bands]
+            resized_filter = Image.merge('RGBA', bands)
+
+            resized_emoji.paste(resized_filter, (0, 0), resized_filter)
+            await self.save_img_and_send(resized_emoji, ctx.channel, file_name="gaygaygay.png")
+            downloaded_emoji.close()
+            gay_filter.close()
+            resized_emoji.close()
+            resized_filter.close()
 
     @commands.command(aliases=["inv"])
     async def invert(self, ctx, inv_object: typing.Union[discord.Member, discord.Emoji]=None):
         if not inv_object or isinstance(inv_object, discord.Member):
-            member = ctx.author if not inv_object else inv_object
+            member = inv_object or ctx.author
             downloaded_pfp = image_from_url(member.avatar_url, "image/png")
             resized_pfp = resize(downloaded_pfp, 250)
 
@@ -50,7 +75,16 @@ class Filters(commands.Cog):
             resized_pfp.close()
         else:
             downloaded_emoji = image_from_url(inv_object.url, "image/png")
-            resized_emoji = resize(downloaded_emoji, 250)
+
+            bands = downloaded_emoji.split()
+            width_diff = abs(downloaded_emoji.size[0] - 250)
+            height_diff = abs(downloaded_emoji.size[1] - 250)
+            if height_diff > width_diff:
+                size = (250, downloaded_emoji.size[1] * 250 // downloaded_emoji.size[0])
+            else:
+                size = (downloaded_emoji.size[0] * 250 // downloaded_emoji.size[1], 250)
+            bands = [b.resize(size, Image.LINEAR) for b in bands]
+            resized_emoji = Image.merge('RGBA', bands)
 
             bands = resized_emoji.split()
             bands = [ImageOps.invert(b) for b in bands[:-1]] + [bands[-1]]
