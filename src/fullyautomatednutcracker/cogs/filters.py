fd7a6e9ca2b32812472519206ebd8a4a7d02f808
dovedevic <dovedevic@gmail.com>
2020-08-24 03:59:50 -0400

    added inv command

--- src/fullyautomatednutcracker/cogs/filters.py
+++ src/fullyautomatednutcracker/cogs/filters.py
@@ -1,17 +1,26 @@
 import discord
 import io
+import typing
 
 from discord.ext import commands
-from PIL import Image
+from PIL import Image, ImageOps
 
 from utils.common_imaging import image_from_url, resize
+
 
 class Filters(commands.Cog):
     """Filters!"""
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=["gay","rainbow"])
+    @staticmethod
+    async def save_img_and_send(img: Image, channel: discord.TextChannel, file_name="filter.png", quality=100):
+        bytes_fp = io.BytesIO()
+        img.save(bytes_fp, format="PNG", quality=quality)
+        bytes_fp.seek(0)
+        await channel.send(file=discord.File(bytes_fp, file_name))
+
+    @commands.command(aliases=["gay", "rainbow"])
     async def gaygaygay(self, ctx, member: discord.Member =None):
         member = ctx.author if not member else member
         downloaded_pfp = image_from_url(member.avatar_url, "image/png")
@@ -19,16 +28,38 @@ class Filters(commands.Cog):
         filter = Image.open('content/filters/gaygaygay.png')
         resized_filter = resize(filter, 250)
         resized_pfp.paste(resized_filter, (0, 0), resized_filter)
-        bytes_fp = io.BytesIO()
-        resized_pfp.save(bytes_fp, quality=95, format="PNG")
-        bytes_fp.seek(0)
+        await self.save_img_and_send(resized_pfp, ctx.channel, file_name="gaygaygay.png")
         downloaded_pfp.close()
         filter.close()
         resized_pfp.close()
         resized_filter.close()
-        await ctx.channel.send(file=discord.File(bytes_fp, f"pfp_filter.png"))
+
+    @commands.command(aliases=["inv"])
+    async def invert(self, ctx, inv_object: typing.Union[discord.Member, discord.Emoji]=None):
+        if not inv_object or isinstance(inv_object, discord.Member):
+            member = ctx.author if not inv_object else inv_object
+            downloaded_pfp = image_from_url(member.avatar_url, "image/png")
+            resized_pfp = resize(downloaded_pfp, 250)
+
+            bands = resized_pfp.split()
+            bands = [ImageOps.invert(b) for b in bands[:-1]] + [bands[-1]]
+            resized_pfp = Image.merge('RGBA', bands)
+
+            await self.save_img_and_send(resized_pfp, ctx.channel, file_name="invert.png")
+            downloaded_pfp.close()
+            resized_pfp.close()
+        else:
+            downloaded_emoji = image_from_url(inv_object.url, "image/png")
+            resized_emoji = resize(downloaded_emoji, 250)
+
+            bands = resized_emoji.split()
+            bands = [ImageOps.invert(b) for b in bands[:-1]] + [bands[-1]]
+            resized_emoji = Image.merge('RGBA', bands)
+
+            await self.save_img_and_send(resized_emoji, ctx.channel, file_name="invert.png")
+            downloaded_emoji.close()
+            resized_emoji.close()
 
 
 def setup(bot):
     bot.add_cog(Filters(bot))
-    
