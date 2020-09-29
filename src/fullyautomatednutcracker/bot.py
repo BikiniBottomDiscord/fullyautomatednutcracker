80c004f18d10dcd8cde4714aed1fd249136b700a
AxoManAxo <52941588+AxoManAxo@users.noreply.github.com>
2020-09-29 12:16:17 -0500

    add intents but you're also 117 commits behind smh

--- src/fullyautomatednutcracker/bot.py
+++ src/fullyautomatednutcracker/bot.py
@@ -20,7 +20,7 @@ logging.basicConfig(level=logging.INFO)
 
 logging.info("Starting \"Fully Automated Nutcracker\" bot script!")
 _pfx = ["$", "dmb ", "<:daddy:590058725252005888> "]
-bot = commands.Bot(command_prefix=_pfx)
+bot = commands.Bot(command_prefix=_pfx, intents=discord.Intents.all())
 bot.help_command = commands.MinimalHelpCommand()
 # bot.is_owner = is_owner  # TODO: do we want this? @dove
 version = "v0.0.1b"
