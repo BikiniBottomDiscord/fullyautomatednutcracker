c15229e824cb9030feb164d921994638c86abfcb
AxoManAxo <52941588+AxoManAxo@users.noreply.github.com>
2020-11-22 23:53:31 -0600

    Reverted bot.py and fun.py changes

--- src/fullyautomatednutcracker/bot.py
+++ src/fullyautomatednutcracker/bot.py
@@ -22,7 +22,7 @@ logging.basicConfig(level=logging.INFO)
 
 logging.info("Starting \"Fully Automated Nutcracker\" bot script!")
 _pfx = ["$", "dmb ", "<:daddy:590058725252005888> "]
-bot = commands.Bot(command_prefix=_pfx, case_insensitive=True, intents=discord.Intents.all())
+bot = commands.Bot(command_prefix=_pfx, intents=discord.Intents.all(), case_insensitive=True)
 bot.help_command = CommunityHelpCommand()
 # bot.is_owner = is_owner  # TODO: do we want this? @dove
 version = "v0.0.1b"
